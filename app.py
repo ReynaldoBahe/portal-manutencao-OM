bd2981a48b0998740bb05abc7a7f5799c804f295
ReynaldoBahe <reynaldo.bahe1@gmail.com>
2026-06-25 22:33:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -221,8 +221,8 @@ if arquivo_upload is not None and not df_exibicao.empty:
         
                 df_fechadas_resp = df_mes[df_mes['Status'] == 'Fechado']
                 if not df_fechadas_resp.empty:
-            produtividade = df_fechadas_resp['Responsavel'].value_counts()
-            st.bar_chart(produtividade)
+                produtividade = df_fechadas_resp['Responsavel'].value_counts()
+                st.bar_chart(produtividade)
                 else:
             st.info("Nenhuma ordem fechada encontrada no filtro selecionado para montar o gráfico de barras.")
 
